e5136ebbfa5506da56383dabde62f4f893262046
nayoungee <nyny637@gmail.com>
2022-09-02 11:07:38 +0900

    Initial commit

---
+++ src/main/java/hello/springmvc/basic/LogTestController.java
@@ -0,0 +1,28 @@
+package hello.springmvc.basic;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LogTestController {
+
+    private final Logger log = LoggerFactory.getLogger(LogTestController.class);
+                                                        // 현재 나의 클래스 getClass() 라고 써도 됨
+
+    @RequestMapping("/log-test")
+    public String logTest(){
+        String name = "Spring";
+
+        System.out.println("name = " + name);
+
+        log.trace("trace log={}", name);
+        log.debug("debug log={}", name);
+        log.info(" info log={}", name);
+        log.warn(" warn log={}", name);
+        log.error("error log={}", name);
+
+        return "ok";
+    }
+}
